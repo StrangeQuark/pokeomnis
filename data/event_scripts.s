6db032f450a765b1019b13612fb872232044933d
StrangeQuark <zacharyzirkle@hotmail.com>
2026-01-22 23:32:28 -0600

    Change Mauville gym to use Lavaridge layout

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1696,3 +1696,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/MauvilleCity_Gym_B1F/scripts.inc"
