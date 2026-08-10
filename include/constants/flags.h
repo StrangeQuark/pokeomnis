6b50fa8ccd67b8a744bdafb798043884227a465c
StrangeQuark <zacharyzirkle@hotmail.com>
2026-08-10 13:25:49 -0500

    Remove some unused flags

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -904,7 +904,7 @@
 #define FLAG_HIDE_BATTLE_TOWER_MULTI_BATTLE_PARTNER_ALT_1           0x360
 #define FLAG_HIDE_BATTLE_TOWER_MULTI_BATTLE_PARTNER_ALT_2           0x361
 #define FLAG_HIDE_PETALBURG_GYM_WALLY                               0x362
-#define FLAG_UNKNOWN_0x363                                          0x363 // Set, however has no purpose.
+#define FLAG_UNUSED_0x363                                           0x363
 #define FLAG_HIDE_LITTLEROOT_TOWN_FAT_MAN                           0x364
 #define FLAG_HIDE_SLATEPORT_CITY_STERNS_SHIPYARD_MR_BRINEY          0x365
 #define FLAG_HIDE_LANETTES_HOUSE_LANETTE                            0x366
@@ -952,7 +952,7 @@
 #define FLAG_UNUSED_0X390                                           0x390
 #define FLAG_UNUSED_0X391                                           0x391
 #define FLAG_UNUSED_0X392                                           0x392
-#define FLAG_UNKNOWN_0x393                                          0x393 // Set, however has no purpose.
+#define FLAG_UNUSED_0X393                                           0x393
 #define FLAG_UNUSED_0X394                                           0x394
 #define FLAG_UNUSED_0X395                                           0x395
 #define FLAG_HIDE_BATTLE_TOWER_REPORTER                             0x396
