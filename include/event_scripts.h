11816c731d3ddd773d4d07f366df4f86e40bf36d
StrangeQuark <zacharyzirkle@hotmail.com>
2026-08-12 18:25:41 -0500

    Remove Scott SS Tidal flag

--- include/event_scripts.h
+++ include/event_scripts.h
@@ -550,7 +550,6 @@ extern const u8 EventScript_FieldPoison[];
 extern const u8 EventScript_EggHatch[];
 extern const u8 AbnormalWeather_EventScript_EndEventAndCleanup_1[];
 extern const u8 IslandCave_EventScript_OpenRegiEntrance[];
-extern const u8 LittlerootTown_ProfessorBirchsLab_EventScript_ScottAboardSSTidalCall[];
 extern const u8 RustboroCity_Gym_EventScript_RegisterRoxanne[];
 extern const u8 SSTidalCorridor_EventScript_ReachedStepCount[];
 extern const u8 EventScript_FallDownHoleMtPyre[];
